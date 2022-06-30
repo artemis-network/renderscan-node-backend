f94285cc820c575fdec113d10c6f9c90d4c86480
artemis-network <artemis-network>
2022-06-30 12:26:46 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ docker stop renderscan-backend-server
 docker rm renderscan-backend-server
 docker rmi renderscan/backend-server
 docker build --no-cache -t renderscan/backend-server .
-docker run -p 7000:7000 -p 587:587 --restart=always --name renderscan-backend-server -d --env-file .env renderscan/backend-server
+docker run -p 5001:5001 -p 587:587 --restart=always --name renderscan-backend-server -d --env-file .env renderscan/backend-server
 docker system prune -a -f
 docker ps -a
